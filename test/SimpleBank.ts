c1c73850047cf352bc67984f1d338c0989406408
Julian Murphy <njmdistrisoft@gmail.com>
2022-08-08 00:48:36 -0300

    deposit

--- test/SimpleBank.ts
+++ test/SimpleBank.ts
@@ -16,8 +16,11 @@ describe("SimpleBank", function () {
 
   describe("Deployment", function () {
     it("Should set the right owner", async function () {
+
       const { simpleBank, owner } = await loadFixture(deployOneYearLockFixture);
+
       expect(await simpleBank.owner()).to.equal(owner.address);
+
     });
 
   });
@@ -27,7 +30,14 @@ describe("SimpleBank", function () {
     it("Should receive and store the funds to lock", async function () {
 
       const { simpleBank, owner, testAccountOne} = await loadFixture(deployOneYearLockFixture);
-      await expect(simpleBank.connect(testAccountOne).deposit({ from: testAccountOne.address, value: 500000})).to.revertedWith("need enrollment")
+
+      await expect(
+        simpleBank.connect(testAccountOne).deposit(
+          {
+            from: testAccountOne.address,
+            value: 500000
+          }
+        )).to.revertedWith("need enrollment")
 
     });
 
@@ -39,6 +49,28 @@ describe("SimpleBank", function () {
       expect(enrollment).to.emit(simpleBank, "LogEnrolled")
 
       expect(await simpleBank.connect(testAccountOne).getBalance()).to.equal(0)
+
+    });
+
+  })
+  describe("Deposit", function() {
+
+    it("Should deposit 50000 to balance and change ether balance", async function () {
+
+      const { simpleBank, testAccountOne} = await loadFixture(deployOneYearLockFixture);
+
+      const enrollment = await simpleBank.connect(testAccountOne).enroll()
+
+      expect(enrollment).to.emit(simpleBank, "LogEnrolled")
+
+      const amountToDeposit = 50000
+
+      expect(await simpleBank.connect(testAccountOne).deposit({from: testAccountOne.address, value: amountToDeposit}))
+        .to.emit(simpleBank, "LogDepositMade")
+        .to.changeEtherBalance(testAccountOne, amountToDeposit)
+
+      expect(await simpleBank.connect(testAccountOne).getBalance())
+        .to.equal(amountToDeposit)
 
     });
   })
